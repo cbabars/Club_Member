75107f825002987fe15b4c63e2ce964da001de5c
Sagar Babar <sagarbabar.sb@gmail.com>
2024-07-05 11:40:03 +0530

    added empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BusinessDirectoryComponent } from 'src/components/business-directory/business-directory.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    BusinessDirectoryComponent
+    BusinessDirectoryComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
